692b1acbf6daf1f31aa3e51ccbb9ac1e6f2e016a
Roman <roma2255@bk.ru>
2024-03-03 00:48:27 +0400

    check-questions route added

--- python-api/api.py
+++ python-api/api.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -6,9 +7,14 @@ def check_questions():
     data = request.json
     correct_words = data.get('correct_words')
     input_words = data.get('input_words')
+    all_mistakes: list[list[int]] = []
     for correct_word, input_word in zip(correct_words, input_words):
-        pass
-    return 'Hello World!', 200
+        mistakes = []
+        for i in range(len(correct_word)):
+            if input_word[i] != correct_word[i]:
+                mistakes.append(i)
+        all_mistakes.append(mistakes)
+    return json.dumps(all_mistakes), 200
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
