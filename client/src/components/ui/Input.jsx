00ccda42f220cd5c5a51ae32d9337bdb622a692a
DmitryKolbanov <kolbanov.dmitry@yandex.ru>
2024-03-03 20:12:42 +0400

    corrected input

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-
 export default function Input(props) {
-    const [value, setValue] = useState("");
-
     return (
-        <>
+        <div className="px-4 py-2 text-lg">
             <label htmlFor={props.label}>{props.label}:</label>
             <input
                 id={props.label}
                 type="text"
-                className="border border-solid rounded py-2 px-4 w-full m-1"
-                value={value}
-                onChange={(event) => setValue(event.target.value)}
+                className="w-full px-4 py-2 m-1 border border-solid rounded focus:outline-none focus:border-2 focus:border-accent"
+                value={props.value}
+                onChange={(event) => props.setValue(event.target.value)}
             />
-        </>
+        </div>
     );
 }
