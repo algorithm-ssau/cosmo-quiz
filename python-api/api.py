8ecd860eebbb63542f61455b13288143211a1cf6
Roman <roma2255@bk.ru>
2024-03-03 14:43:09 +0400

    create letters array added

--- python-api/api.py
+++ python-api/api.py
@@ -1,6 +1,8 @@
-import json
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import random
+
 app = Flask(__name__)
+
 
 @app.route('/check-questions', methods=['GET'])
 def check_questions():
@@ -8,13 +10,27 @@ def check_questions():
     correct_words = data.get('correct_words')
     input_words = data.get('input_words')
     all_mistakes: list[list[int]] = []
-    for correct_word, input_word in zip(correct_words, input_words):
+    for correct_word, input_word in zip(correct_words, input_words, strict=True):
         mistakes = []
         for i in range(len(correct_word)):
-            if input_word[i] != correct_word[i]:
+            if input_word[i].lower() != correct_word[i].lower():
                 mistakes.append(i)
         all_mistakes.append(mistakes)
-    return json.dumps(all_mistakes), 200
+    return jsonify(all_mistakes), 200
+
+
+@app.route('/create', methods=['GET'])
+def create_array():
+    data = request.json
+    words: list[str] = data.get('words')
+    letters: list[str] = []
+    for word in words:
+        letters.extend(list(word.upper()))
+    while len(letters) % len(words) != 0:
+        letters.append(chr(random.randint(1040, 1071)))
+    random.shuffle(letters)
+    return jsonify(letters), 200
+
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
