fa1d596fb19519682397bf8e847d9c49683fc222
Danila <officialdannisimo@gmail.com>
2024-04-29 15:21:27 +0400

    Client routes fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { useEffect } from 'react';
 import { checkAuth } from './store/slices/authSlice';
-import { Link, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Private from './routes/Private';
 import Public from './routes/Public';
 
 export default function App() {
-  // const dispatch = useDispatch();
-  // const isAuth = useSelector(state => state.auth.isAuth);
-  // const isAuthLoading = useSelector(state => state.auth.isLoading);
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuth);
+  const isAuthLoading = useSelector(state => state.auth.isLoading);
 
-  // useEffect(() => {
-  //   dispatch(checkAuth());
-  // }, [dispatch]);
-
-  const isAuth = true;
-  const isAuthLoading = false;
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
 
   return (
     <HelmetProvider>
