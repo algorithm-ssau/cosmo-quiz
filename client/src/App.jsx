153f567c0736ff51ae2a1f5550faea3b2f377ca7
Danila <officialdannisimo@gmail.com>
2024-04-29 15:20:10 +0400

    Client - routes fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { useEffect } from 'react';
 import { checkAuth } from './store/slices/authSlice';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Private from './routes/Private';
 import Public from './routes/Public';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const isAuth = useSelector(state => state.auth.isAuth);
-  const isAuthLoading = useSelector(state => state.auth.isLoading);
+  // const dispatch = useDispatch();
+  // const isAuth = useSelector(state => state.auth.isAuth);
+  // const isAuthLoading = useSelector(state => state.auth.isLoading);
 
-  useEffect(() => {
-    dispatch(checkAuth());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(checkAuth());
+  // }, [dispatch]);
+
+  const isAuth = true;
+  const isAuthLoading = false;
 
   return (
     <HelmetProvider>
@@ -28,15 +31,15 @@ export default function App() {
                 {Private.map(elem => {
                   <Route key={elem.path} path={elem.path} Component={elem.component} />;
                 })}
+                <Route path='*' element={<Navigate to='/' replace={true} />} />
               </Route>
-              <Route path='*' element={<Navigate to='/' replace={true} />} />
             </Routes>
           ) : (
             <Routes>
               {Public.map(elem => {
                 <Route key={elem.path} path={elem.path} Component={elem.component} />;
+                <Route path='*' element={<Navigate to='/login' replace={true} />} />;
               })}
-              <Route path='*' element={<Navigate to='/login' replace={true} />} />
             </Routes>
           )}
         </>
