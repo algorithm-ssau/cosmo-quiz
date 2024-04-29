e8ce1ade3b2b361faedc0a96d495dbbf2a04add7
DmitryKolbanov <kolbanov.dmitry@yandex.ru>
2024-04-29 12:32:00 +0400

    fix AuthService

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -1,27 +1,28 @@
-import http from './AxiosService';
+import http from "./AxiosService";
 
 class AuthService {
-  async register(email, password) {
-    return http.post('/api/auth/register', {
-      email: email,
-      password: password,
-    });
-  }
+    async register(name, email, password) {
+        return http.post("/api/auth/register", {
+            name: name,
+            email: email,
+            password: password,
+        });
+    }
 
-  async login(email, password) {
-    return http.post('/api/auth/login', {
-      email: email,
-      password: password,
-    });
-  }
+    async login(email, password) {
+        return http.post("/api/auth/login", {
+            email: email,
+            password: password,
+        });
+    }
 
-  async refresh() {
-    return http.post('/api/auth/refresh');
-  }
+    async refresh() {
+        return http.post("/api/auth/refresh");
+    }
 
-  async logout() {
-    return http.post('/api/auth/logout');
-  }
+    async logout() {
+        return http.post("/api/auth/logout");
+    }
 }
 
 export default new AuthService();
