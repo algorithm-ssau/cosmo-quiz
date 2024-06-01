ae54f5cf1f352f9c1cafcfff5b5126e561d13d40
Danila <officialdannisimo@gmail.com>
2024-06-01 04:13:41 +0400

    express-api: topic service fix

--- express-api/src/controllers/QuestionController.ts
+++ express-api/src/controllers/QuestionController.ts
@@ -1,23 +1,34 @@
-import { NextFunction, Request, Response } from "express";
-import { Types, isObjectIdOrHexString } from "mongoose";
-import ApiError from "../errors/ApiError";
-import QuestionService from "../services/QuestionService";
+import { NextFunction, Request, Response } from 'express';
+import { Types, isObjectIdOrHexString } from 'mongoose';
+import ApiError from '../errors/ApiError';
+import QuestionService from '../services/QuestionService';
+import TQuestion from '../types/Question';
 
 class QuestionController {
   async get(req: Request, res: Response, next: NextFunction) {
     try {
       const question_id_param = req.params.id;
       if (!isObjectIdOrHexString(question_id_param)) {
-        throw ApiError.BadRequest("Неверный id");
+        throw ApiError.BadRequest('Неверный id');
       }
       const question_id = new Types.ObjectId(question_id_param);
 
       const question = await QuestionService.get(question_id);
       if (!question) {
-        throw ApiError.BadRequest("Неверный id");
+        throw ApiError.BadRequest('Неверный id');
       }
 
-      res.status(200).json(question);
+      res
+        .status(200)
+        .json({
+          _id: question._id,
+          answer: question.answer,
+          author: question.author,
+          name: question.name,
+          question: question.question,
+          answerVideo: question.answerVideo,
+          questionVideo: question.questionVideo,
+        });
     } catch (error) {
       next(error);
     }
